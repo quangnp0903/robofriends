6addf9280692e2ce486f029cdfd25b45c8ab5dea
Quang Pham <quangnp0903@gmail.com>
2025-01-03 16:05:25 +0700

    create robofriends react app

---
+++ src/components/CardList.js
@@ -0,0 +1,17 @@
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot) => (
+        <Card
+          key={robot.id}
+          id={robot.id}
+          name={robot.name}
+          email={robot.email}
+        />
+      ))}
+    </div>
+  );
+};
+export default CardList;
