85d6135504341e29254611b7e309449141a730fd
Quang Pham <quangnp0903@gmail.com>
2025-02-15 20:51:58 +0700

    implement redux with robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,45 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 // import { robots } from './robots';
 import SearchBox from '../components/SearchBox';
-
 import './App.css';
 import Scroll from '../components/Scroll';
+import { requestRobots, setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: '',
-    };
-  }
-
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
-
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  };
 
   render() {
-    const { robots, searchField } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter((robot) =>
       robot.name.toLowerCase().includes(searchField)
     );
 
-    return !robots.length ? (
+    return isPending ? (
       <h1>Loading...</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
@@ -45,4 +48,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
