b3f10344504474929b6cc6fc1f1cd1247362ce4f
Quang Pham <quangnp0903@gmail.com>
2025-02-19 15:12:20 +0700

    implementing pwa and favicon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import { thunk } from 'redux-thunk';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'tachyons';
 
 import './index.css';
@@ -23,6 +24,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
