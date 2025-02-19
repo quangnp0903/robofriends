3cab4226015f16c31debb142fba3a713e9e03188
Quang Pham <quangnp0903@gmail.com>
2025-02-19 10:06:35 +0700

    update code

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import { requestRobots, setSearchField } from '../actions';
+import Header from '../components/Header';
 
 const mapStateToProps = (state) => {
   return {
@@ -38,7 +39,7 @@ class App extends Component {
       <h1>Loading...</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <Header />
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
